193df19341b6ad6f66c365619e6e68d5a5e6ce20
Raghav chawla <raghchawla.131@gmail.com>
2023-09-14 00:09:34 +0530

    nav mobile view

---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+export default function Navbar() {
+  return (
+    <>
+      <header>
+        <h1 className="logo">Logo</h1>
+        <input type="checkbox" id='nav-toggle' className='nav-toggle'/>
+        <label htmlFor="nav-toggle" className='nav-toggle-label'>
+          <span><ion-icon name="menu-outline"></ion-icon></span>
+        </label>
+        <nav>
+          <ul>
+            <li>
+              <a href="i">Home</a>
+            </li>
+            <li>
+              <a href="j">About us</a>
+            </li>
+            <li>
+              <a href="l">Contact us</a>
+            </li>
+            <li>
+              <a href="h">Shop</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
