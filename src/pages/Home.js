f5ac8759dd72052406218a41a190123357e799b0
Raghav chawla <raghchawla.131@gmail.com>
2023-09-16 14:05:17 +0530

    page-3,4,5

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,15 @@
+import HorizontalScroller from "../components/HorizontalScroller";
+import cakeSvg from "../assets/cake.svg";
+import boxSvg from "../assets/box.svg";
+import tempImg from "../assets/tempImg.jpg";
+
 export default function Home() {
   return (
     <div id="home">
       <div className="page-1">
         <div className="image-pair">
-          <div id="image-pair-first"></div>
-          <div id="image-pair-second"></div>
+          <img className="image-pair-first" src={tempImg} alt="" />
+          <img className="image-pair-second" src={tempImg} alt="" />
         </div>
         <div className="shop-now-content">
           <h1>DESSERTS DELIVERED TO YOUR DOOR</h1>
@@ -26,66 +31,71 @@ export default function Home() {
         </div>
       </div>
       <div className="page-2">
-        <div className="page-2-text">
+        <div className="horizontal-scroller-header">
           <h1>FROM OUR BAKERY TO YOUR DOORSTEP</h1>
-          <p>Treat your friends, fam, and even your coworkers to limited-edition and best-selling treats. Ships in 1-2 Days!</p>
+          <p>
+            Treat your friends, fam, and even your coworkers to limited-edition
+            and best-selling treats. Ships in 1-2 Days!
+          </p>
         </div>
-        <div className="page-2-scroll-items">
-          <div className="scroll-items">
-            <div className="scroll-items-top">
-              <img src="https://images.unsplash.com/photo-1565958011703-44f9829ba187?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1965&q=80" alt="" />
-              </div>
-            <div className="scroll-items-bottom">
-              <h4>BIRTHDAY CAKE</h4>
-              <p>Our bestselling Birthday Cake, inspired by the boxed vanilla funfetti...</p>
-            </div>
+        <HorizontalScroller />
+      </div>
+      <div className="page-3">
+        <h2>
+          WHY CHOOSE <span>RAB DI RASOI?</span>
+        </h2>
+        <div className="perks">
+          <div className="perk-1">
+            <img src={cakeSvg} alt="" />
+            <h4>ONE-OF-A-KIND TREATS</h4>
+            <p>
+              Try our award-winning classic Birthday Cake and our fan-favorite
+              B’Day Cake Truffles!
+            </p>
           </div>
-          <div className="scroll-items">
-            <div className="scroll-items-top">
-              <img src="https://images.unsplash.com/photo-1484723091739-30a097e8f929?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1547&q=80" alt="" />
-              </div>
-            <div className="scroll-items-bottom">
-              <h4>BIRTHDAY CAKE</h4>
-              <p>Our bestselling Birthday Cake, inspired by the boxed vanilla funfetti...</p>
-            </div>
-          </div>
-          <div className="scroll-items">
-            <div className="scroll-items-top">
-              <img src="https://images.unsplash.com/photo-1664192579012-1e98c2ddd779?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1888&q=80" alt="" />
-              </div>
-            <div className="scroll-items-bottom">
-              <h4>BIRTHDAY CAKE</h4>
-              <p>Our bestselling Birthday Cake, inspired by the boxed vanilla funfetti...</p>
-            </div>
-          </div>
-          <div className="scroll-items">
-            <div className="scroll-items-top">
-              <img src="https://images.unsplash.com/photo-1484980972926-edee96e0960d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80" alt="" />
-              </div>
-            <div className="scroll-items-bottom">
-              <h4>BIRTHDAY CAKE</h4>
-              <p>Our bestselling Birthday Cake, inspired by the boxed vanilla funfetti...</p>
-            </div>
-          </div>
-          <div className="scroll-items">
-            <div className="scroll-items-top">
-              <img src="https://images.unsplash.com/photo-1484723091739-30a097e8f929?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1547&q=80" alt="" />
-              </div>
-            <div className="scroll-items-bottom">
-              <h4>BIRTHDAY CAKE</h4>
-              <p>Our bestselling Birthday Cake, inspired by the boxed vanilla funfetti...</p>
-            </div>
-          </div>
-          <div className="scroll-items">
-            <div className="scroll-items-top">
-              <img src="https://images.unsplash.com/photo-1565958011703-44f9829ba187?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1965&q=80" alt="" />
-              </div>
-            <div className="scroll-items-bottom">
-              <h4>BIRTHDAY CAKE</h4>
-              <p>Our bestselling Birthday Cake, inspired by the boxed vanilla funfetti...</p>
-            </div>
+          <div className="perk-2">
+            <img src={boxSvg} alt="" />
+            <h4>ONE-OF-A-KIND TREATS</h4>
+            <p>
+              Try our award-winning classic Birthday Cake and our fan-favorite
+              B’Day Cake Truffles!
+            </p>
           </div>
         </div>
+      </div>
+      <div className="page-4">
+        <div className="image-pair">
+          <img className="image-pair-first" src={tempImg} alt="" />
+          <img className="image-pair-second" src={tempImg} alt="" />
+        </div>
+        <div className="shop-now-content">
+          <h1>SUMMER FUN, DELIVERED.</h1>
+          <p>
+            We’ve spent hours in the kitchen channeling our favorite flavors of
+            summer into the layers of buttery, bright cake. Our new Strawberry
+            Corn Cake brings the culinary “if it grows together, it goes
+            together” rule straight to your doorstep in 1-2 days. We
+            thoughtfully package all of our treats so they arrive perfectly and
+            ready to enjoy. Better get those sunglasses ready :)
+          </p>
+          <a href="j">ORDER NOW</a>
+        </div>
+        <div className="infinite-scroll">
+          <p>SHAKING UP THE DESSET SCENE SINCE 2021.</p>
+          <p>SHAKING UP THE DESSET SCENE SINCE 2021.</p>
+          <p>SHAKING UP THE DESSET SCENE SINCE 2021.</p>
+          <p>SHAKING UP THE DESSET SCENE SINCE 2021.</p>
+          <p>SHAKING UP THE DESSET SCENE SINCE 2021.</p>
+        </div>
+      </div>
+      <div className="page-5">
+        <div className="horizontal-scroller-header">
+          <h1>SHOP OUR BESTSELLERS</h1>
+          <p>
+          Iconic layer cakes with unfrosted sides, gooey pie, and fudgy Cake Truffles.
+          </p>
+        </div>
+        <HorizontalScroller />
       </div>
     </div>
   );
