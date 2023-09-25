7966e2fd5d5c6dd511a9c762aad1206376c7d25e
Raghav chawla <raghchawla.131@gmail.com>
2023-09-25 17:33:00 +0530

    nav large screen

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,32 +4,29 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <>
-      <header>
-        <img className="logo" src={logo} alt="" />
-        <input type="checkbox" id="nav-toggle" className="nav-toggle" />
-        <label htmlFor="nav-toggle" className="nav-toggle-label">
-          <span>
-            <ion-icon name="menu-outline"></ion-icon>
-          </span>
-        </label>
-        <nav>
+      <nav>
+        <ion-icon name="menu-outline"></ion-icon>
+        <div id="logo">
+          <img className="logo" src={logo} alt="" />
+        </div>
+        <ion-icon name="cart-outline"></ion-icon>
+        <div className="nav-links">
           <ul>
             <li>
               <Link to="/">Home</Link>
             </li>
             <li>
-              <Link to="/About">About us</Link>
+              <Link to="/Shop">Shop</Link>
             </li>
             <li>
               <Link to="/Contact">Contact us</Link>
             </li>
             <li>
-              <Link to="/Shop">Shop</Link>
+              <Link to="/About">About us</Link>
             </li>
           </ul>
-        </nav>
-        <hr />
-      </header>
+        </div>
+      </nav>
     </>
   );
 }
