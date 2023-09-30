f7b813c01f3ab62b3482e95fd70fc58b4c9f336d
Raghav chawla <raghchawla.131@gmail.com>
2023-09-30 12:26:48 +0530

    HorizontalScroller

--- src/components/HorizontalScroller.js
+++ src/components/HorizontalScroller.js
@@ -2,10 +2,10 @@ export default function HorizontalScroller({ data }) {
   return (
     <>
       <div className="horizontal-scroller">
-        {data.map((item, key) => (
-          <div className="scroll-items" key={key}>
+        {data.map((item) => (
+          <div className="scroll-items" key={item.id}>
             <div className="scroll-items-top">
-              <img src={item.image} alt="productImage" />
+              <img key={item.id} src={item.image} alt="productImage" />
             </div>
             <div className="scroll-items-bottom">
               <h4>{item.title}</h4>
