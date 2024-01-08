b6bfd55af8f358b284b47700b5fcdb0903b6b350
Raghav chawla <raghchawla.131@gmail.com>
2024-01-09 00:03:24 +0530

    footer css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,84 +5,66 @@ export default function Footer() {
     <>
       <footer>
         <div className="shop-footer">
-          <div>
-            <div className="footer-socials">
-              <h1>Let's keep in touch!</h1>
-              <p>
-                Find us on any of these platforms, we respond 1-2 business days.
-              </p>
-            </div>
-            <div className="footer-page-links">
-              <div>
-                <table className="table-1">
-                  <thead>
-                    <tr>
-                      <th>USEFUL LINKS</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>About Us</td>
-                    </tr>
-                    <tr>
-                      <td>Blog</td>
-                    </tr>
-                    <tr>
-                      <td>Github</td>
-                    </tr>
-                    <tr>
-                      <td>Free Products</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-              <div>
-                <table className="table-2">
-                  <thead>
-                    <tr>
-                      <th>OTHER RESOURCES</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>About Us</td>
-                    </tr>
-                    <tr>
-                      <td>Blog</td>
-                    </tr>
-                    <tr>
-                      <td>Github</td>
-                    </tr>
-                    <tr>
-                      <td>Free Products</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
-          <div></div>
-        </div>
-
-        {/* <div className="shop-footer">
-          <div className="footer-a">
-            <h1>INVITE MILK BAR TO YOUR B’DAY PARTY</h1>
+          <div className="footer-socials">
+            <h1>Let's keep in touch!</h1>
             <p>
-              Tell us your birthday + receive a special treat with your birthday
-              order :)
+              Find us on any of these platforms, we respond 1-2 business days.
             </p>
-            <form className="footer-newsletter">
-              <input type="text" placeholder="Your email" />
-              <input type="text" placeholder="Your birthday" />
-              <button>SUBMIT</button>
-            </form>
-            <div className="socials">
-              <ion-icon name="logo-instagram"></ion-icon>
+            <section className="footer-socials-section">
+              <Socials icon={<ion-icon name="logo-instagram"></ion-icon>} />
+              <Socials icon={<ion-icon name="logo-whatsapp"></ion-icon>} />
+              <Socials icon={<ion-icon name="call-outline"></ion-icon>} />
+            </section>
+          </div>
+          <div className="footer-page-links">
+            <div>
+              <table className="table-1">
+                <thead>
+                  <tr>
+                    <th>USEFUL LINKS</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>About Us</td>
+                  </tr>
+                  <tr>
+                    <td>Blog</td>
+                  </tr>
+                  <tr>
+                    <td>Github</td>
+                  </tr>
+                  <tr>
+                    <td>Free Products</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div>
+              <table className="table-2">
+                <thead>
+                  <tr>
+                    <th>OTHER RESOURCES</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>About Us</td>
+                  </tr>
+                  <tr>
+                    <td>Blog</td>
+                  </tr>
+                  <tr>
+                    <td>Github</td>
+                  </tr>
+                  <tr>
+                    <td>Free Products</td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
           </div>
-          <div className="footer-b"></div>
-          <div className="footer-c"></div>
-        </div> */}
+        </div>
       </footer>
     </>
   );
--- src/components/socials.jsx
+++ src/components/socials.jsx
@@ -1,3 +1,7 @@
-export default function Socials() {
-  
+export default function Socials({ icon }) {
+  return (
+    <div className="socials">
+      {icon}
+    </div>
+  );
 }
