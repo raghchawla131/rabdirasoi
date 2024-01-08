1ebec2867a73146cde9b6be5798f05bd75ac39a3
Raghav chawla <raghchawla.131@gmail.com>
2024-01-08 20:22:02 +0530

    footer struct

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,70 @@
+import Socials from "./socials";
+
 export default function Footer() {
   return (
     <>
       <footer>
         <div className="shop-footer">
+          <div>
+            <div className="footer-socials">
+              <h1>Let's keep in touch!</h1>
+              <p>
+                Find us on any of these platforms, we respond 1-2 business days.
+              </p>
+            </div>
+            <div className="footer-page-links">
+              <div>
+                <table className="table-1">
+                  <thead>
+                    <tr>
+                      <th>USEFUL LINKS</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>About Us</td>
+                    </tr>
+                    <tr>
+                      <td>Blog</td>
+                    </tr>
+                    <tr>
+                      <td>Github</td>
+                    </tr>
+                    <tr>
+                      <td>Free Products</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div>
+                <table className="table-2">
+                  <thead>
+                    <tr>
+                      <th>OTHER RESOURCES</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>About Us</td>
+                    </tr>
+                    <tr>
+                      <td>Blog</td>
+                    </tr>
+                    <tr>
+                      <td>Github</td>
+                    </tr>
+                    <tr>
+                      <td>Free Products</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+          <div></div>
+        </div>
+
+        {/* <div className="shop-footer">
           <div className="footer-a">
             <h1>INVITE MILK BAR TO YOUR B’DAY PARTY</h1>
             <p>
@@ -20,7 +82,7 @@ export default function Footer() {
           </div>
           <div className="footer-b"></div>
           <div className="footer-c"></div>
-        </div>
+        </div> */}
       </footer>
     </>
   );
