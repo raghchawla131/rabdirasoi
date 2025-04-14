474a5227b6a16155cffecb573774d9f6d2fc263c
Raghav chawla <raghchawla.131@gmail.com>
2025-04-14 15:00:57 +0530

    npm cron keep alive req for render

--- server/app.js
+++ server/app.js
@@ -7,8 +7,11 @@ const productsRoutes = require('./routes/products');
 const cartRoutes = require('./routes/cart');
 const customerDetails = require('./routes/customerDetails');
 const razorpay = require('./routes/razorpay');
+const keepAlive = require('./routes/keepAlive');
 const path = require('path');
 const dotenv = require('dotenv');
+const cron = require('node-cron');
+const axios = require('axios');
 
 // Load environment variables from .env file
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
@@ -33,6 +36,15 @@ app.use('/api/products', productsRoutes);
 app.use('/api/cart', cartRoutes);
 app.use('/api/customerDetails', customerDetails);
 app.use('/api/payment', razorpay);
+app.use('/api/keep-alive', keepAlive);
+
+// Cron job to send a "keep-alive" request every 14 minutes
+cron.schedule('*/14 * * * *', () => {
+  console.log('Sending keep-alive request');
+  axios.get(`http://localhost:${PORT}/api/keep-alive`) // Ensure you have this route
+    .then(response => console.log('Server is alive!'))
+    .catch(err => console.error('Error with keep-alive request:', err));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
