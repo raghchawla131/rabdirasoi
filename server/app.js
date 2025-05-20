ff2357fdc3686b12b66cd4b6d335b14cda269b30
Raghav chawla <raghchawla.131@gmail.com>
2025-05-20 08:31:34 +0530

    fixes based on old commits

--- server/app.js
+++ server/app.js
@@ -1,20 +1,16 @@
-const express = require('express');
-const db = require('./db');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const authRoutes = require('./routes/auth');
-const productsRoutes = require('./routes/products');
-const cartRoutes = require('./routes/cart');
-const customerDetails = require('./routes/customerDetails');
-const razorpay = require('./routes/razorpay');
-const keepAlive = require('./routes/keepAlive');
-const path = require('path');
-const dotenv = require('dotenv');
-const cron = require('node-cron');
-const axios = require('axios');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const authRoutes = require("./routes/auth");
+const productsRoutes = require("./routes/products");
+const cartRoutes = require("./routes/cart");
+const customerDetails = require("./routes/customerDetails");
+const razorpay = require("./routes/razorpay");
+const path = require("path");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
 // Load environment variables from .env file
-dotenv.config({ path: path.resolve(__dirname, '../.env') });
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
 const app = express();
 
@@ -31,19 +27,14 @@ app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/auth', authRoutes);
-app.use('/api/products', productsRoutes);
-app.use('/api/cart', cartRoutes);
-app.use('/api/customerDetails', customerDetails);
-app.use('/api/payment', razorpay);
-app.use('/api/keep-alive', keepAlive);
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productsRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/customerDetails", customerDetails);
+app.use("/api/payment", razorpay);
 
-// Cron job to hit a real product endpoint every 14 minutes
-cron.schedule('*/14 * * * *', () => {
-  console.log('⏰ Sending keep-alive DB-touch request');
-  axios.post(`https://rabdirasoi.onrender.com/api/products/get-product/keepalive`) // Replace with a real product ID from your DB
-    .then(res => console.log('✅ Keep-alive successful:', res.status))
-    .catch(err => console.error('❌ Keep-alive failed:', err.message));
+app.post("/", (req, res) => {
+  res.json({ message: "Hello from server!" });
 });
 
 app.listen(PORT, () => {
--- server/routes/keepAlive.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.status(200).send("server is alive");
-})
-
-module.exports = router;
